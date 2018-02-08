6ed2cfad2afd52365a4de38549c41d388551e0ca
Daniel STANCU <daniel.stancu@me.com>
2018-02-08 19:17:02 +0200

    Fix YML twig extension service broken load path

--- src/DependencyInjection/AvatarizeExtension.php
+++ src/DependencyInjection/AvatarizeExtension.php
@@ -28,6 +28,6 @@ class AvatarizeExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('avatarize.yml');
+        $loader->load('twig.yml');
     }
 }
